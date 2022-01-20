4ec4d79b0fea9992fe1be5ed54efcb1404321b7b
Elenaperd24 <elena24.perdomo@gmail.com>
2022-01-20 11:27:15 -0300

    con navbar-responsive-color menu

--- src/components/NavbarPrueba.js
+++ src/components/NavbarPrueba.js
@@ -5,35 +5,43 @@ import logo from "../image/logos/logoMyTinerary.png"
 import socialmedia from "../image/logos/socialmedia.png"
 import logoNegro from "../image/logos/logoNegro.png"
 import login from "../image/logos/login.png";
+import { screen } from "@testing-library/react";
 
 function NavbarPrueba() {
     const [colorChange, setColorchange] = useState(false);
     const [colorLogo, setColorLogo] = useState(true);
+    const [color, setColor] = useState("prueba");
+   
     const changeNavbarColor = () => {
-        if (window.scrollY >= 50) {
+        if (window.scrollY > 100) {
             setColorchange(true);
             setColorLogo(false);
-        }
+                   }
         else {
             setColorchange(false);
             setColorLogo(true);
-        }
-    };
-    window.addEventListener('scroll', changeNavbarColor);
+        }      
+       
+    };  
+    
 
+
+
+    window.addEventListener('scroll', changeNavbarColor);
+    
 
     return (
         <>
-            <nav className={colorChange ? " colorChange navbar shadow navbar-expand-md navbar-light fixed-top" :
-                "navbar navbar-expand-md shadow navbar-light fixed-top"}>
-                <div className="container-fluid">
+            <nav className={colorChange ? "colorChange navbar shadow navbar-expand-md navbar-light  fixed-top " :
+                "navbar navbar-expand-md shadow navbar-light fixed-top prueba"}>
+                <div className="container-fluid p-0">
                     <img src={colorLogo ? logo : logoNegro} className="navbar-brand logo img-fluid" alt="brand" />
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+                    <button class="navbar-toggler"  type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
-                    <div id="navbarToggler" className="collapse navbar-collapse">
+                    <div id="navbarToggler" className={"collapse navbar-collapse " + color}>
                         <div className="mx-auto"></div>
-                        <ul className="navbar-nav">
+                        <ul className="navbar-nav ">
                             <li className="nav-item" >
                                 <LinkRouter to="/" className="nav-link">
                                     <button className="btn btn-nav" >HOME</button>
