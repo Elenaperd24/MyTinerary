c66dd93067a842342268ce37d6e270acf4d0e497
Elenaperd24 <elena24.perdomo@gmail.com>
2022-04-06 22:42:45 -0300

    :(

--- server.js
+++ server.js
@@ -11,8 +11,9 @@ const HOST = process.env.HOST || "0.0.0.0"
 require('./config/database')
 require('./config/passport')
 //middlewares
-app.use(cors());
 app.use(passport.initialize())
+app.use(cors());
+
 app.use(express.json());
 app.use('/api',Router)
 
